618345a89ce64fbff5c0e31227f3bc046f46c991
strongwarmi <elianasc10@hotmail.com>
2017-05-02 17:35:25 -0500

    cambiando colo a los items del menu cuando nav es blanco

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,19 @@ var lastScrollTop = 0;
      var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
      if (currentScroll > lastScrollTop){
        document.getElementById("nav-header").classList.add("header-in");
-       //document.getElementById("nav-header").style.opacity = 0;
+       Drive.style.color = "#333447";
+       Explore.style.color = "#333447";
+       Help.style.color = "#333447";
+       Login.style.color = "#333447";
      }
      else {
        document.getElementById("nav-header").classList.add("header-in");
        if(currentScroll<=3){
          document.getElementById("nav-header").classList.remove("header-in");
+         Drive.style.color = "#FFF";
+         Explore.style.color = "#FFF";
+         Help.style.color = "#FFF";
+         Login.style.color = "#FFF";
        }
      }
      lastScrollTop = currentScroll;
